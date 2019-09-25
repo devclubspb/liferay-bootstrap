8699668bf48c3f66af389569b7e8cfcbf1a2a145
Grig Alex <alexengrigdev@gmail.com>
2019-09-25 19:13:49 +0300

    Fix package name to entry-api module

--- modules/entry/entry-api/src/main/java/io/github/devclubspb/liferay/bootstrap/portlet/entry/api/EntryService.java
+++
@@ -1,7 +0,0 @@
-package io.github.devclubspb.liferay.bootstrap.portlet.entry.api;
-
-/**
- * @author Grig Alex
- */
-public interface EntryService {
-}
---
+++ modules/entry/entry-api/src/main/java/ru/spb/devclub/liferay/bootstrap/portlet/entry/api/EntryService.java
@@ -0,0 +1,7 @@
+package ru.spb.devclub.liferay.bootstrap.portlet.entry.api;
+
+/**
+ * @author Grig Alex
+ */
+public interface EntryService {
+}
